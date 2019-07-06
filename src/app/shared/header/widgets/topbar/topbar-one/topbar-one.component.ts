22e66e6f8a84f45566de5ffb3f47a2838daf9121
mossiXYZ <mossi.hashemi@gmail.com>
2019-07-06 13:04:28 +0200

    288 implementing Logout

--- src/app/shared/header/widgets/topbar/topbar-one/topbar-one.component.ts
+++ src/app/shared/header/widgets/topbar/topbar-one/topbar-one.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../../../../classes/product';
 import { WishlistService } from '../../../../services/wishlist.service';
 import { ProductsService } from '../../../../../shared/services/products.service';
 import { Observable, of } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-topbar',
@@ -10,9 +11,17 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./topbar-one.component.scss']
 })
 export class TopbarOneComponent implements OnInit {
-  
-  constructor(public productsService: ProductsService) { }
+
+  constructor(
+    public productsService: ProductsService,
+    private afAuth: AngularFireAuth
+  ) {
+    afAuth.authState.subscribe(x => console.log(x));
+  }
 
   ngOnInit() { }
 
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
